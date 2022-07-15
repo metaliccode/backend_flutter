c87c4c8ceee6bd1c0b50d549af078f8650e7358c
metaliccode <isan.mh69@gmail.com>
2022-07-15 10:06:54 +0700

    rubah web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return 'Hello world';
-    return view('welcome');
+    return view('welcome test');
 });
 
 // Route::get(
